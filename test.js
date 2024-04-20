f80ec4e0f7dc77d6e4b2295b4f8dff47d3ea975b
yuliahapz <yulia.hapz@gmail.com>
2024-04-20 20:05:31 +0700

    first commit

--- test.js
+++ test.js
@@ -11,6 +11,6 @@ const calc = (angka1, angka2, operator) => {
     }
 }
     console.log (calc(7, 7, "+"));
-    console.log (calc(9, 2, "-"));
+    console.log (calc(10, 2, "-"));
     console.log (calc(6, 7, "*"));
     console.log (calc(10, 2, "/"));
