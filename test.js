b9a6b5a22c671517cb5a20a71ed8dd4c24df471a
yuliahapz <yulia.hapz@gmail.com>
2024-04-20 17:51:13 +0700

    pesan commit

--- test.js
+++ test.js
@@ -10,7 +10,7 @@ const calc = (angka1, angka2, operator) => {
             return angka1 / angka2;
     }
 }
-    console.log (calc(5, 7, "+"));
+    console.log (calc(7, 7, "+"));
     console.log (calc(9, 2, "-"));
     console.log (calc(6, 7, "*"));
     console.log (calc(10, 2, "/"));
